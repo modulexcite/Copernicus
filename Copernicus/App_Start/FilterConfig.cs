4808fd4e03061be044b55694d38f89c478dad497
James Craig <admin@gutgames.com>
2013-11-12 22:02:22 -0500

    Added initial items

---
+++ Copernicus/App_Start/FilterConfig.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Copernicus
+{
+    /// <summary>
+    /// Filter config
+    /// </summary>
+    public static class FilterConfig
+    {
+        /// <summary>
+        /// Registers the global filter
+        /// </summary>
+        /// <param name="filters">Filter collection</param>
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            if (filters == null)
+                return;
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Copernicus/Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Copernicus
+{
+    /// <summary>
+    /// HTTP Application class
+    /// </summary>
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        /// <summary>
+        /// Application start
+        /// </summary>
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
