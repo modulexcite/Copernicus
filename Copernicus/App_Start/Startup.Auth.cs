a47fd92cfdbc56bc57c3ce0598066ac0bdd8faa7
James Craig <admin@gutgames.com>
2014-04-09 21:43:51 -0400

    Updated provider code a bit.

--- Copernicus/App_Start/Startup.Auth.cs
+++ Copernicus/App_Start/Startup.Auth.cs
@@ -1,11 +1,4 @@
-﻿using Copernicus.Core.Providers;
-using Copernicus.Models.Authentication;
-using Copernicus.Models.Authentication.Stores;
-using Microsoft.AspNet.Identity;
-using Microsoft.Owin;
-using Microsoft.Owin.Security.Cookies;
-using Owin;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Security.Claims;
@@ -14,6 +7,13 @@ using System.Threading.Tasks;
 using System.Web;
 using System.Web.Configuration;
 using System.Web.Mvc;
+using Copernicus.Core.Providers;
+using Copernicus.Models.Authentication;
+using Copernicus.Models.Authentication.Stores;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin;
+using Microsoft.Owin.Security.Cookies;
+using Owin;
 using Utilities.DataTypes;
 
 namespace Copernicus
@@ -31,39 +31,26 @@ namespace Copernicus
         {
             if (app == null)
                 return;
-            var Config = WebConfigurationManager.OpenWebConfiguration("/");
-            var AuthSection = (AuthenticationSection)Config.GetSection("system.web/authentication");
-            if (AuthSection.Mode == AuthenticationMode.Forms)
-            {
-                // Enable the application to use a cookie to store information for the signed in user
-                app.UseCookieAuthentication(new CookieAuthenticationOptions
-                {
-                    AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
-                    LoginPath = new PathString("/Account/Login"),
-                    LogoutPath = new PathString("/Account/LogOff")
-                });
-                // Use a cookie to temporarily store information about a user logging in with a
-                // third party login provider
-                app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
-            }
-            else if (AuthSection.Mode == AuthenticationMode.Windows)
-            {
-                app.Use(typeof(WindowsPrincipalHandler));
-            }
-
-            //app.UseMicrosoftAccountAuthentication(
-            // clientId: "",
-            // clientSecret: "");
-
-            //app.UseTwitterAuthentication(
-            // consumerKey: "",
-            // consumerSecret: "");
-
-            //app.UseFacebookAuthentication(
-            // appId: "",
-            // appSecret: "");
-
-            //app.UseGoogleAuthentication();
+            Utilities.IoC.Manager.Bootstrapper.Register<IAppBuilder>(app);
+            //var Config = WebConfigurationManager.OpenWebConfiguration("/");
+            //var AuthSection = (AuthenticationSection)Config.GetSection("system.web/authentication");
+            //if (AuthSection.Mode == AuthenticationMode.Forms)
+            //{
+            //    // Enable the application to use a cookie to store information for the signed in user
+            //    app.UseCookieAuthentication(new CookieAuthenticationOptions
+            //    {
+            //        AuthenticationType = DefaultAuthenticationTypes.ApplicationCookie,
+            //        LoginPath = new PathString("/Account/Login"),
+            //        LogoutPath = new PathString("/Account/LogOff")
+            //    });
+            //    // Use a cookie to temporarily store information about a user logging in with a
+            //    // third party login provider
+            //    app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
+            //}
+            //else if (AuthSection.Mode == AuthenticationMode.Windows)
+            //{
+            //    app.Use(typeof(WindowsPrincipalHandler));
+            //}
         }
     }
 }
